706b0b9ed89da9821aeb238073756391d8fd9428
CarlosDnlExfinity <carlos.ospina@exfinity.com.co>
2022-02-11 11:30:51 -0500

    Cadena de caracteres

--- Course.js
+++ Course.js
@@ -46,6 +46,118 @@ console.log(a)
 a = 56;
 console.log(a);
 
+var suma = 7 + 12; //Sumando valores
+console.log(suma);
+var resta = 75-10; //Restando valores
+console.log(resta);
+var multiplicacion = 45*75; //Multiplicación
+console.log(multiplicacion);
+var division = 45 / 7; //División
+console.log(division);
+var modulo = 45 % 7; //Sobrande
+console.log(modulo); 
+var dividirCero = 8 / 0;  //Dividir entre 0
+console.log(dividirCero);
+
+//Números decimales
+var miNumeroDecimal = 78.25;
+var peso = 80;
+
+var productoDecimal = 3.4 * 77.8;
+console.log(productoDecimal);
+
+var decimalEntero = 6 * 8.9;
+console.log(decimalEntero);
+
+var productoCero = 45 *0;
+console.log(productoCero);
+
+var decimalNegativo = 45 * -85;
+console.log(decimalNegativo);
+
+var dividirDecimal = 4.2 / 8.4;
+console.log(dividirDecimal);
+
+var decimalCero = 4.2 / 0;
+console.log(decimalCero);
+
+var resto = 15 % 5;
+console.log(resto);
+
+//Incrementar valor de las variables
+var librosComprados = 105;
+
+//Opción 1
+librosComprados = librosComprados + 1;
+//Opción 2
+librosComprados++;
+
+//Reducir el valor de una variable
+var numeroDeEstudiantes = 45;
+
+//Opción 1
+numeroDeEstudiantes = numeroDeEstudiantes -1;
+//Opcion 2
+numeroDeEstudiantes--;
+
+//Incrementar valor en x cantidad
+var incrementarCantidad = 30;
+incrementarCantidad +=5;
+console.log(incrementarCantidad);
+
+var totalVentas = 15478.3;
+console.log(totalVentas);
+totalVentas += 78542.2;
+console.log(totalVentas);
+
+
+//Reducir valor x cantidad
+var reducirCantidad = 45;
+reducirCantidad -= 10;
+console.log(reducirCantidad);
+
+var totalDeuda = 457852;
+console.log(totalDeuda);
+totalDeuda -= 45;
+console.log(totalDeuda);
+
+
+
+//Multiplicar x cantidad
+var multiplicarIncremento = 200;
+multiplicarIncremento *= 10;
+
+
+var salario = 45000;
+console.log(salario);
+salario *= 5;
+console.log(salario);
+
+//Dividir x cantidad
+var dividirCantidad = 39;
+dividirCantidad /= 2;
+console.log(dividirCantidad);
+
+var salarioDividir = 45000;
+salarioDividir /= 2;
+console.log(salarioDividir);
+
+//Cadena de caracteres
+var nombre = "Carlos";
+var apellido = 'Orlando';
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
