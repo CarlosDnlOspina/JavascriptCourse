54f1c0d88291836bcd4178bb4026dcacabae06e3
CarlosDnlExfinity <carlos.ospina@exfinity.com.co>
2022-02-11 10:53:13 -0500

    Inicialización de variables

--- Course.js
+++ Course.js
@@ -7,11 +7,58 @@
 */ 
 
 
+//Asignar valor a variables
+
 var miVariable = "freeCodeCamp";
 console.log(miVariable);
 
 miVariable = 16;
 console.log(miVariable);
+
+miVariable = true;
+console.log(miVariable);
+
+var miUsuario = "CarlosDnlOspina";
+console.log(miUsuario);  //La variable de se debe llamar con el mismo nombre que fue asignada
+
+//Declarar variables
+var a;
+var b = 2;
+
+console.log(a); // undefined
+console.log(b);  // Tiene un valor
+
+// =  Es el operador de asignación en Javascript
+a = 10;
+console.log(a);
+
+//Asignando el valor de la varaible a en la variable b
+b = a;
+console.log(b);
+
+
+var x = 9; //Inicializando la variable
+var miIdioma = "Español";
+
+
+var a; //Variable no inicializada
+console.log(a)
+a = 56;
+console.log(a);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
