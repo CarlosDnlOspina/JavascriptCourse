51cb16324df99c11f871daf41fd19b8e3613a3d2
CarlosDnlExfinity <carlos.ospina@exfinity.com.co>
2022-02-11 10:42:09 -0500

    Subiendo la guía

---
+++ Course.js
@@ -0,0 +1,18 @@
+//Comentario de una sola línea
+
+/*
+ Este es un comentario de varias líneas
+ Tipos de Datos:
+ undefined, null, boolean, string, symbol, number y object
+*/ 
+
+
+var miVariable = "freeCodeCamp";
+console.log(miVariable);
+
+miVariable = 16;
+console.log(miVariable);
+
+
+
+
